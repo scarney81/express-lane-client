e4bb78d1fa342a4672b9fab78466e07cdce4a2ac
Troy Goode <troygoode@gmail.com>
2012-04-16 21:33:15 -0400

    implemented orders

--- repositories/orders.js
+++ repositories/orders.js
@@ -13,7 +13,50 @@ module.exports = function(config) {
     },
 
     add: function(emailAddress, productsInCart, cb){
-      cb(null, null);
+      if(!emailAddress || !emailAddress.length){
+        cb('no email address');
+        return;
+      }
+      if(!productsInCart || !productsInCart.length){
+        cb('no products in cart');
+        return;
+      }
+
+      var order = {
+        email: emailAddress,
+        products: productsInCart.map(function(productInCart){
+          return {
+            _id: productInCart.product._id,
+            image: productInCart.product.image,
+            name: productInCart.product.name,
+            price: productInCart.product.price,
+            quantity: productInCart.quantity
+          };
+        }),
+        total_price: productsInCart.reduce(function(prev, cur){
+          return prev + (cur.quantity * cur.product.price);
+        }, 0),
+        billing: { //TODO?
+          cc_no: '',
+          cvs: '',
+          exp_date: '',
+          name: '',
+          zip: ''
+        },
+        shipping: { //TODO?
+          address: '',
+          city: '',
+          state: '',
+          zip: ''
+        }
+      };
+
+      var url = config.serverUrl + '/orders';
+      var options = { json: true, method: 'post', url: url, body: order };
+      request(options, function(err, res, body) {
+        if (err !== null) cb(err);
+        else cb(null, body);
+      });
     },
 
     mark_complete: function(id, cb) {
