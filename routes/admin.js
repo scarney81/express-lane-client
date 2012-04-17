9ce7884e5707ef7a70e2261d6214fc376afc648c
Troy Goode <troygoode@gmail.com>
2012-04-17 00:08:08 -0400

    fixing merge

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,23 @@
+function requireAdminRights(req, res, next){
+    if(!req.session || !req.session.auth || !req.session.auth.user || !req.session.auth.user.username)
+      res.send('Unauthorized', 401);
+    else if(req.session.auth.user.username !== 'SethCarney')
+      res.send('Forbidden', 403);
+    else
+      next();
+}
+
 module.exports = function(app, ordersRepo) {
 
   app.get('/admin', requireAdminRights, function(req, res){
     ordersRepo.find(null, function(err, orders){
       if(err) throw err;
-      res.render('admin', { orders: orders });
+      var total = orders.length;
+      var pending = orders.filter(function(order) { return order.status === 'pending'; });
+      var complete = orders.filter(function(order) { return order.status === 'complete'; });
+      var pending_percentage = (pending.length/total)*100;
+      var complete_percentage = (complete.length/total)*100;
+      res.render('admin', { orders: orders, total: total, pending: pending.length, complete: complete.length, pending_percentage: pending_percentage, complete_percentage: complete_percentage});
     });
   });
 
